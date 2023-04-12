cd59cd17dc3a073fb1ad994ddd18b30f2898fdb3
BayzuAriya <bayzuariya@gmail.com>
2023-04-12 10:31:55 +0800

    capek

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -17,8 +17,6 @@
     <script src="{{ asset('js/kml.js') }}"></script>
     <script src="{{ asset('js/main.js') }}"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-    <script src="https://unpkg.com/leaflet-omnivore@0.4.9/leaflet-omnivore.min.js"></script>
 </head>
 
 <body id="body-pd">
